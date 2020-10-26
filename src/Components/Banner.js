f53fc638771e9976b8e23e1a1c075c02c60c9597
Torrence Brown <torrenceb90@gmail.com>
2020-10-25 18:04:37 -0700

    Added styling to banner/app background

--- src/Components/Banner.js
+++ src/Components/Banner.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import '../Banner.css'
+import "../Banner.css";
 import axios from "../axios";
 import requests from "../requests";
 
@@ -20,6 +20,10 @@ const Banner = () => {
     // console.log(`Movie: ${movie}`);
   }, []);
 
+  const truncate = (str, number) => {
+    return str?.length > number ? str.substr(0, number - 1) + "..." : str;
+  };
+
   return (
     <header
       className="banner"
@@ -33,13 +37,18 @@ const Banner = () => {
         {/* Some Api's don't always give you all the information
           that you need, therefore it's really important 
           to find the edge cases and implement them. */}
-        <h1>{movie?.title || movie?.name || movie?.original_name}</h1>
+        <h1 className="banner__title">
+          {movie?.title || movie?.name || movie?.original_name}
+        </h1>
         <div className="banner__buttons">
           <button className="banner__button">Play</button>
           <button className="banner__button">My List</button>
         </div>
-        <h1 className="banner__description">{movie?.overview}</h1>
+        <h1 className="banner__description">
+          {truncate(movie?.overview, 150)}
+        </h1>
       </div>
+      <div className="banner--fadeBottom"></div>
     </header>
   );
 };
