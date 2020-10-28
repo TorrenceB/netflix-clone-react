7e3e4d68c827dfa060a1e7a4cecb847ce9382fb6
Torrence Brown <torrenceb90@gmail.com>
2020-10-27 21:25:03 -0700

    Added movie trailer and youtube packages

--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -1,9 +1,22 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "../Nav.css";
 
 const Nav = () => {
+  const [show, handleShow] = useState(false);
+
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      if (window.scrollY > 100) {
+        handleShow(true);
+      } else handleShow(false);
+    });
+    return () => {
+      window.removeEventListener("scroll");
+    };
+  }, []);
+
   return (
-    <div className="nav">
+    <div className={`nav ${show && "nav__black"}`}>
       <img
         className="nav__logo"
         src="https://upload.wikimedia.org/wikipedia/commons/0/08/Netflix_2015_logo.svg"
