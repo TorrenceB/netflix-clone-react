e760cae261da37ff2a2eb3762bd5848531e5fb71
Torrence Brown <torrenceb90@gmail.com>
2020-11-07 12:00:28 -0800

    Set user row width

--- src/Components/SelectUser.js
+++ src/Components/SelectUser.js
@@ -6,6 +6,32 @@ const SelectUser = () => {
   return (
     <div className="wrapper">
       <Nav></Nav>
+      <div className="wrapper__content">
+        <h1 className="wrapper__title">Who's Watching?</h1>
+        <div className="wrapper__user__row">
+          <div className="wrapper__user__col">
+            <img
+              className="wrapper__avatar"
+              src="https://i.pinimg.com/originals/0d/dc/ca/0ddccae723d85a703b798a5e682c23c1.png"
+            ></img>
+            <h3 className="wrapper__username">Torrence</h3>
+          </div>
+          <div className="wrapper__user__col">
+            <img
+              className="wrapper__avatar"
+              src="https://i.pinimg.com/originals/0d/dc/ca/0ddccae723d85a703b798a5e682c23c1.png"
+            ></img>
+            <h3 className="wrapper__username">Torrence</h3>
+          </div>
+          <div className="wrapper__user__col">
+            <img
+              className="wrapper__avatar"
+              src="https://i.pinimg.com/originals/0d/dc/ca/0ddccae723d85a703b798a5e682c23c1.png"
+            ></img>
+            <h3 className="wrapper__username">Torrence</h3>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
